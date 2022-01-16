f7df6428698ba07d8edc454709e730124d7b3815
ZP <zairaperez01@gmail.com>
2022-01-16 17:40:40 -0600

    final additions

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+var  selectEl = document.querySelector("#select");
+var todayWeatherEl = document.querySelector("#today");
+var apiKey = "fb2240ea04f3fe606d4e76d86a8a7ae3"
+var cityInputEl = document.querySelector("#city");
+var citySearchWeatherEl = document.querySelector("#city-search-weather");
+var searchHistoryEl = document.querySelector("#search-history");
+
+var cityUser = function (event) {
+    event.preventDefault();
+
+    var userWord = cityInputEl.value.trim()
+    getUserCity(cityInputEl);
+}
+var searchCity = function () {
+    var resultsTyped = "http://api.openweathermap.org/data/2.5/weather?q=chicago,illinois,840" + city + "&appid=" + apiKey;
+    fetch(resultsTyped).then(function(response) {
+        if(response.ok) {
+            console.log(response);
+       response.json().then(function(data) {
+           console.log(data)
+           citySearchWeatherEl.textContent = city;
+        getUserCity(data, city); 
+       })
+    } else    
+    alert("Error: No city was found");
+    
+    })
+    .catch(function(error) {
+        alert("Unable to find a matching city");
+    })
+    
+}
+var getUserCity = function() {
+    var apiWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=chicago,840" + city + "&appid=" + apiKey;
+
+    
+    fetch(apiWeatherUrl).then(function(response) {
+        if(response.ok) {
+            console.log(response);
+       response.json().then(function(data) {
+           console.log(data)
+        todayWeather(data, city); 
+    })
+
+}
+ else 
+    alert("Error: No city was found");
+    
+    })
+    .catch(function(error) {
+        alert("Unable to find a matching city");
+    })
+}
+
+var todayWeather = function(data, cityInputEl) {
+
+ for (var i = 0; i < city.length; i++) {
+    var dt = data[i]  
+
+     var citySearchWeatherEl = document.createElement("span");
+     citySearchWeatherEl.classList= "list-item";
+
+     var textEl = document.createElement("div");
+     textEl.textContent = city;
+
+     citySearchWeatherEl.appendChild(textEl);
+
+     searchHistoryEl.appendChild(citySearchWeatherEl);
+
+
+
+
+
+ }
+}
+
+getUserCity();
+    
+
+selectEl.addEventListener("submit", todayWeather);
+
